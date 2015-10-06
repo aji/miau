fcde1d1c8742fc866d3f0a5b087ba5bb65d78eb3
Alex Iadicicco <alex@ajitek.net>
2015-10-06 01:47:01 -0700

    Add a doc string to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
+//! `miau` is a community IRC bot with a continuous delivery workflow allowing
+//! participants to have their changes deployed to the running instance within
+//! minutes.
+
+/// entry point!
+pub fn main() {
     println!("Hello, world!");
 }
